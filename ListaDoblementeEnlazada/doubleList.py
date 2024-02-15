026b2b716366163f26ef491ba3e308ce49526e7e
MRobertoUMG <mrompichy@miumg.edu.gt>
2024-02-15 16:18:36 -0600

    insert de Nodos

---
+++ ListaDoblementeEnlazada/doubleList.py
@@ -0,0 +1,3 @@
+class doubleList:
+    def __init__(self):
+        self.root = None
--- ListaDoblementeEnlazada/nodo.py
+++ ListaDoblementeEnlazada/nodo.py
@@ -1,2 +1,48 @@
-print("Mi primer nodo")
-print("Lista Doblemente Enlazada")
+class Nodo:
+    def __init__(self, data):
+        self.name = data["name"]
+        self.surname = data["surname"]
+        self.id = data["id"]
+        self.sig = None
+        self.ant = None
+
+class ListaDoble:
+    #root
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+
+    def insertarAlPrincipio(self, data):
+        nuevoNodo = Nodo(data)
+        nuevoNodo.sig = self.cabeza
+        self.cabeza = nuevoNodo
+
+    def insertarAlFinal(self, data):
+        nuevoNodo = Nodo(data)
+        nuevoNodo.ant = self.cola
+        self.cola = nuevoNodo
+    
+    def imprimir(self):
+        #insertPrincipio
+        actual = self.cabeza
+        while actual:
+            print(actual.name, end=" -> ")
+            actual = actual.sig
+        #insertFinal
+        ultimo = self.cola
+        while ultimo:
+            print(ultimo.name, end=" -> ")
+            ultimo = ultimo.ant
+        print("None")
+
+
+nodo = ListaDoble()
+nodo.insertarAlPrincipio({"name":"Mario", "surname":"Rompich", "id":213})
+nodo.insertarAlPrincipio({"name":"Jonh", "surname":"Jerez", "id":257})
+nodo.insertarAlPrincipio({"name":"nathy", "surname":"Jerez", "id":257})
+
+nodo.insertarAlFinal({"name":"Astrid", "surname":"Archila", "id":2045})
+nodo.imprimir()
+
+
+
