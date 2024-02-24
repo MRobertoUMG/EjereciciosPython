62034bb544a357e7bf5a652785bcc54541dd90ac
MRobertoUMG <mrompichy@miumg.edu.gt>
2024-02-23 22:11:27 -0600

    readMe y menu de opciones

---
+++ Recursividad/readMe.md
@@ -0,0 +1,18 @@
+RECURSIVIDAD
+
+
+Instrucciones:
+Al momento de ejecutar el programa en consola CLI este deplegará el siguiente menu:
+
+1-Convertir a Binario
+2-Contar Dígitos
+3-Raíz Cuadrada Entera
+4-Convertir a Decimal desde Romano
+5-Suma de Números Enteros
+6-Salir
+
+En el cual en base a ingresar un numero del 1 al 6 este ejecutara un metodo para hacer una accion que se desea.Dependiendo que opción elijamos nos solicita un parametro el cual tenemos que introducir para pasarselo a la función.
+
+APOYO EN EL DASARROLLO
+
+Mario Roberto Rompich Yoc 9490-17-17052 - 100%
--- Recursividad/recursividad.py
+++ Recursividad/recursividad.py
@@ -18,7 +18,7 @@ def contarDigitos(numero):
         return 1 + contarDigitos(numero // 10)
 
 #print(contarDigitos(4822102))
-
+#-------------------------------------------------------------------------
 #Raíz Cuadrada Entera:
 def raizCuadradaEntera(num, candidato=0):
   
@@ -45,13 +45,53 @@ def romanoADecimal(romano):
     else:
         return romanos[romano[0]] + romanoADecimal(romano[1:])
 #print(romanoADecimal("XV"))
-
+#-------------------------------------------------------------------------
 #Suma de Números Enteros:
 def SumaNumerosEnteros(numero):
-    if numero == 1:
+    if numero == 1: 
         return 1
     else:
         return numero + SumaNumerosEnteros(numero - 1)
-    
-print(SumaNumerosEnteros(4))
+
+#-------------------------------------------------------------------------
+#print(SumaNumerosEnteros(4))
+
+try:
+    if __name__ == "__main__":
+        opc = 0
+
+        while opc != 6:
+            print("\n------------------------------------------------",)
+            print("**** Menu ****", end='\n')
+            print("Elige una opción (Número) para ingresar hacer una acción")
+            print("-------------------------------------------------")
+            print("1 - Convertir a Binario")
+            print("2 - Contar Dígitos")
+            print("3 - Raíz Cuadrada Entera")
+            print("4 - Convertir a Decimal desde Romano")
+            print("5 - Suma de Números Enteros")
+            print("6 - Salir")
+            opc = int(input("Ingrese su opción(Número): "))
+
+            if opc == 1:
+                numero = int(input("Ingresa el número a convertir en binario: "))
+                print("Número convertido en binario: " + convertiABinario(numero))
+            elif opc == 2:
+                numero = int(input("Ingresa el número que deseas que cuente el numero de dígitos:"))
+                print(f"Número de dígitos: {contarDigitos(numero)}")
+            elif opc == 3:
+               numero = int(input("Ingresa el número que deseas que calcule la raiz cuadrada entera:"))
+               print(f"Raiz cuadrada entera: {raizCuadradaEntera(numero)}")
+            elif opc == 4:
+                numero = str(input("Ingresa el número romano que deseas convertir a decimal:"))
+                print(f"Número en decimal: {romanoADecimal(numero)}")
+            elif opc == 5:
+                numero = int(input("Ingresa el número entero que desees que se sume:"))
+                print(f"La suma de los números enteros: {SumaNumerosEnteros(numero)}")
+            elif opc == 6:
+                print("Sesión Terminada, si necesita ingresar nuevamente ejecute el codigo en CLI")
+            else:
+                print("opción inválida.")
+except Exception as e:
+    print(e)
 
