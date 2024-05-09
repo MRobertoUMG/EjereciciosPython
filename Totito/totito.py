a4c738e9d472a56bc5e7ab94b6306f0771a55901
MRobertoUMG <mrompichy@miumg.edu.gt>
2024-05-09 13:07:26 -0600

    building interface

---
+++ Totito/totito.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter  import  simpledialog
+
+window = Tk()
+
+root = window
+#root.configure(background='black')
+window.geometry("500x500")
+window.title("TOTITO ;)")
+
+turno = 0
+nombre_jugador_1 = ""
+nombre_jugador_2 = ""
+lista_btns = []
+tablero = []
+turno_jugador = StringVar()
+window.mainloop()
