582712ef5e6897122b9799b12c8987f8a8810c26
MRobertoUMG <mrompichy@miumg.edu.gt>
2024-05-17 18:45:28 -0600

    deteccion de casilla seleccionada y asignacion en la lista

--- To-Ti-To/main.py
+++ To-Ti-To/main.py
@@ -27,7 +27,7 @@ class ListaSimple:
         self.cabeza = None
  
     def funcion_iniciar(self):
-         for i in range(1,10):
+         for i in range(0,9):
             nuevo_nodo = Nodo({"posicion": i, "valor": "Null" })
             nuevo_nodo.siguiente = self.cabeza
             self.cabeza = nuevo_nodo
@@ -35,10 +35,13 @@ class ListaSimple:
     def asignar_X_O(self,get_posicion):
         actual1 = self.cabeza
         while actual1:
+           
             if actual1.dato["posicion"] == get_posicion:
                 actual1.dato["valor"] = "X"
-                #print(actual1.dato["posicion"])        
-                #return actual.dato["posicion"]
+                lista_btns[get_posicion].config(bg="white")
+                #lista_btns[get_posicion].config(bg="gray")
+                lista_btns[get_posicion].config(text="X")
+                lista_btns[get_posicion].config(state="disable")
             #else:
                 #print("No esta el valor")
                 #print("---")
@@ -49,10 +52,12 @@ class ListaSimple:
             print(actual.dato["valor"] , end=" -> ")
             actual = actual.siguiente
         print("None")
-
             
         #print("None")
         #print(posicion  mcvv)
+    def bloquear(self):
+        for i in range(0,9):
+            lista_btns[i].config(state="disable")
 
     def retirar_primero(self):
         if self.cabeza:
@@ -79,42 +84,37 @@ mi_lista = ListaSimple()
 #mi_lista.funcion_iniciar()
 
 #BUTTONS
-btn0 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(9))
+btn0 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(0))
 lista_btns.append(btn0)
 btn0.place(x=50,y=50)
-btn1 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(8))
+btn1 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(1))
 lista_btns.append(btn1)
 btn1.place(x=150,y=50)
-btn2 = Button(window, width=8, height=4, bg="#011A27",command=lambda: mi_lista.asignar_X_O(7))
+btn2 = Button(window, width=8, height=4, bg="#011A27",command=lambda: mi_lista.asignar_X_O(2))
 lista_btns.append(btn2)
 btn2.place(x=250,y=50)
-btn3 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(6))
+btn3 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(3))
 lista_btns.append(btn3)
 btn3.place(x=50,y=150)
-btn4 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(5))
+btn4 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(4))
 lista_btns.append(btn4)
 btn4.place(x=150,y=150)
-btn5 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(4))
+btn5 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(5))
 lista_btns.append(btn5)
 btn5.place(x=250,y=150)
-btn6 = Button(window, width=8, height=4,bg="#011A27", command=lambda: mi_lista.asignar_X_O(3))
+btn6 = Button(window, width=8, height=4,bg="#011A27", command=lambda: mi_lista.asignar_X_O(6))
 lista_btns.append(btn6)
 btn6.place(x=50,y=250)
-btn7 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(2))
+btn7 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(7))
 lista_btns.append(btn7)
 btn7.place(x=150,y=250)
-btn8 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(1))
+btn8 = Button(window, width=8, height=4, bg="#011A27", command=lambda: mi_lista.asignar_X_O(8))
 lista_btns.append(btn8)
 btn8.place(x=250,y=250)
 
 mostrar_turno_participante = Label(window, textvariable=turno_jugador).place(x=100, y=20)
 iniciar_juego = Button(window, background="green", borderwidth=0,  fg="white", text="Iniciar juego", width=15, height=3,command=mi_lista.funcion_iniciar()).place(x=130, y=350)
-'''mi_lista.agregar_elemento("Juan")
-mi_lista.agregar_elemento("Maria")
-mi_lista.agregar_elemento("Jose")
-mi_lista.agregar_elemento("Douglas")
-mi_lista.agregar_elemento("Pedro")'''
-print("Muestra la lista inicialmente completa")
+#mi_lista.bloquear()
 mi_lista.imprimir_lista()
 #print("Retira el ultimo elemento de la lista")
 #mi_lista.retirar_ultimo()
